9ce4c8180e53972279ca041b89c49cc4a4a09d9c
Vitor Katakura <thevitorhpk@gmail.com>
2024-06-30 00:55:56 -0300

    feat: get book by genre endpoint

--- src/modules/books/books.module.ts
+++ src/modules/books/books.module.ts
@@ -4,11 +4,12 @@ import { BooksController } from './books.controller';
 import { CreateBook } from './services/create-book.service';
 import { DeleteBook } from './services/delete-book.service';
 import { GetBookById } from './services/get-book-by-id.service';
+import { GetBooksByGenre } from './services/get-books-by-genre.service';
 import { UpdateBook } from './services/update-book.service';
 
 @Module({
   imports: [DatabaseModule],
-  providers: [GetBookById, CreateBook, DeleteBook, UpdateBook],
+  providers: [GetBookById, GetBooksByGenre, CreateBook, DeleteBook, UpdateBook],
   controllers: [BooksController],
 })
 export class BooksModule {}
